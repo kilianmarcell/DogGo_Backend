a612fb8b604893a7f6718bc694b90dac204e9846
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-08 11:58:17 +0100

    User model fillable hozzáadva, user api tesztelve, működik

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    protected $table = 'users';
+    
+    protected $visible = [
+        'username',
+        'email',
+        'permission',
+    ];
+
+    protected $hidden = [
+        'id',
+        'password',
+    ];
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'permission',
+    ];
 }
