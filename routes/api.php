97509671c68e4ce5a0b305d76cb36eff33a8d5c4
Takács Balázs <tbalazs.levente@gmail.com>
2022-02-12 18:12:17 +0100

    Routes v2

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/users', UserController::class);
+Route::apiResource('/users', UserController::class);
 
-Route::resource('/locations', LocationController::class);
+Route::apiResource('/locations', LocationController::class);
 
-Route::resource('/feedbacks', FeedbackController::class);
+Route::apiResource('/feedbacks', FeedbackController::class);
 
-Route::resource('/ratings', RatingController::class);
+Route::apiResource('/ratings', RatingController::class);
