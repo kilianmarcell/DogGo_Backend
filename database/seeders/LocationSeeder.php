4ca94c28d8cf9955127f22235d4480dd2bf0cba1
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-31 20:29:47 +0200

    Hely tesztadatok hozzáadva

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -18,7 +18,7 @@ class LocationSeeder extends Seeder
 
         \App\Models\Location::factory()->create([
             'name' => 'McDonald\'s - Keleti',
-            'description' => 'Nagyon jó',
+            'description' => 'Gyors kiszolgálás, sok ember számára is elég nagy a hely, kiskedvenceink is jól érezték magukat, csendes volt a hely.',
             'lat' => 47.5008947,
             'lng' => 19.0819858,
             'allowed' => false,
@@ -27,7 +27,7 @@ class LocationSeeder extends Seeder
 
         \App\Models\Location::factory()->create([
             'name' => 'Burger King - Keleti',
-            'description' => 'Nagyon jó',
+            'description' => 'Kedves pultosok, nem a leghangosabb hely, kellemes az atmoszférája a helynek, ahhoz képest, hogy a belvárosban van!',
             'lat' => 47.5008272,
             'lng' => 19.0817936,
             'allowed' => false,
@@ -36,7 +36,7 @@ class LocationSeeder extends Seeder
 
         \App\Models\Location::factory()->create([
             'name' => 'KFC Budapest - Keleti',
-            'description' => 'Nagyon jó',
+            'description' => 'Húzták a szájukat, de beengedtek minket kutyával. Az állatokat nem szolgálják ki kajával, nem adnak nekik vizet sem.',
             'lat' => 47.5014546,
             'lng' => 19.0831529,
             'allowed' => true,
@@ -45,7 +45,7 @@ class LocationSeeder extends Seeder
 
         \App\Models\Location::factory()->create([
             'name' => 'Pesti Pipi - Keleti',
-            'description' => 'Nagyon jó',
+            'description' => 'Kellemes volt itteni időtöltésem, külön tálban adtak a kutyáinknak vizet, nagyon jó munkát végeznek az ott dolgozók.',
             'lat' => 47.5012856,
             'lng' => 19.0827999,
             'allowed' => true,
@@ -54,11 +54,56 @@ class LocationSeeder extends Seeder
 
         \App\Models\Location::factory()->create([
             'name' => 'Kutya Egy Hely kávézó/bár/kutya napközi',
-            'description' => 'Nagyon jó',
+            'description' => 'Nagyon kedves volt minenki, mindent érthetően le tudtunk beszélni, minden rendben volt a rendeléssel, a kiskedvencünk is megfelelő ellátást kapott!',
             'lat' => 47.5060349,
             'lng' => 19.07362,
             'allowed' => false,
             'user_id' => 5,
+        ]);
+
+        \App\Models\Location::factory()->create([
+            'name' => 'Városliget',
+            'description' => 'Gyönyörű hely, sok kutyás ember jár erre, kutyáknak külön elkerített részek vannak, családos embereknek bátran tudom ajánlani ezt a helyet!',
+            'lat' => 47.51436501, 
+            'lng' => 19.08524642,
+            'allowed' => true,
+            'user_id' => 3,
+        ]);
+
+        \App\Models\Location::factory()->create([
+            'name' => 'Normafa',
+            'description' => 'A város zaja egyeltalán nem hallatszódik, olyan mintha nem is Budapesten lennék, teljesen elvarázsol ez a hely!',
+            'lat' => 47.5066425292158,
+            'lng' => 18.961865246036687,
+            'allowed' => false,
+            'user_id' => 4,
+        ]);
+
+        \App\Models\Location::factory()->create([
+            'name' => 'Margit-sziget',
+            'description' => 'Kellemes élmény volt körbesétálni a szigetet. Több helyen is megálltunk, számtalan hely alkalmas a kutyafuttatásra, piknikezésre, mindenkinek tudom ajánlani!',
+            'lat' => 47.526773338102664,
+            'lng' => 19.04641313900643,
+            'allowed' => false,
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Location::factory()->create([
+            'name' => 'Naplás-tó',
+            'description' => 'Nagyon nyugodt hely, egy délutáni sétára tökéletes hely. A közelben a dombokon is lehet sétálni, az extrémebb biciklizésre is tökéletes hely.',
+            'lat' => 47.50911699363903, 
+            'lng' => 19.24720391573441,
+            'allowed' => false,
+            'user_id' => 5,
+        ]);
+
+        \App\Models\Location::factory()->create([
+            'name' => 'Népliget',
+            'description' => 'Kellemes csalódást okozott ez a hely, a kicsit hangos háttérzaj ellenére kellemes időt tudtam eltölteni családommal itt.',
+            'lat' => 47.481421467218205,
+            'lng' => 19.10947361517664,
+            'allowed' => false,
+            'user_id' => 4,
         ]);
     }
 }
